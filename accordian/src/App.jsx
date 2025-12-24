5c689fb26edc9078135c7506a558d7e5e7ffdaf8
bhuvan chander <bhargav29feb@gmail.com>
2025-12-24 13:21:30 +0530

    ford validation

---
+++ accordian/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Accordian from './components/Accordian'
+function App() {
+
+
+  return (
+    <>
+      <h1> Accordian: </h1>
+      <Accordian />
+    </>
+  )
+}
+
+export default App
---
+++ accordian/src/data.jsx
@@ -0,0 +1,30 @@
+const data = [
+  {
+    title: 'What is React?',
+    content: 'React is a JavaScript library for building user interfaces.',
+  },
+  {
+    title: 'What is useState?',
+    content:
+      'useState is a React Hook used to manage state in functional components.',
+  },
+  {
+    title: 'What is JSX?',
+    content: 'JSX allows us to write HTML inside JavaScript.',
+  },
+  {
+    title: 'What is React?',
+    content: 'React is a JavaScript library for building user interfaces.',
+  },
+  {
+    title: 'What is useState?',
+    content:
+      'useState is a React Hook used to manage state in functional components.',
+  },
+  {
+    title: 'What is JSX?',
+    content: 'JSX allows us to write HTML inside JavaScript.',
+  },
+];
+
+export default data;
